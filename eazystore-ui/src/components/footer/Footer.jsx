a36173e1719a010a8485b7f63d0aaebae95cc633
Himanshu Kandpal <himanshukp324@gmail.com>
2026-02-16 16:28:29 +0530

    dark mode implementation

--- eazystore-ui/src/components/footer/Footer.jsx
+++ eazystore-ui/src/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ import {faHeart} from "@fortawesome/free-solid-svg-icons";
 export default function Footer() {
     return (
         <>
-            <footer className={"flex justify-center items-center py-4 font-primary text-gray-700"}>
+            <footer className="flex justify-center items-center py-4 font-primary text-gray-700 dark:text-gray-300">
                 Built with
                 <FontAwesomeIcon icon={faHeart}
                                  className={"text-red-600 mx-1 animate-pulse"}
@@ -14,7 +14,7 @@ export default function Footer() {
                 <a href={"https://easybytes.com"}
                    target={"_blank"}
                    rel={"noopener noreferrer"}
-                   className={"text-primary font-semibold px-1 transition-colors duration-300 hover:text-dark"}
+                   className="text-primary dark:text-light font-semibold px-1 transition-colors duration-300 hover:text-dark dark:hover:text-lighter"
                 >
                     eazybytes
                 </a>
--- eazystore-ui/src/components/product/ProductListings.jsx
+++ eazystore-ui/src/components/product/ProductListings.jsx
@@ -1,26 +1,54 @@
 import ProductCard from "./ProductCard.jsx";
 import SearchBox from "../Searchbox.jsx";
 import Dropdown from "../Dropdown.jsx";
-import {useState} from "react";
+import {useMemo, useState} from "react";
+
+const sortList = ["Popularity", "Price Low to High", "Price High to Low"];
 
 export default function ProductListings({products}) {
 
     const [search, setSearch] = useState("");
+    const [sortValue, setSortValue] = useState("Popularity");
 
     function handleSearchChange(inputSearch) {
         setSearch(inputSearch);
         console.log(inputSearch);
     }
 
-    let filteredProducts = Array.isArray(products) ?
-        products.filter(product =>
-            product.name.toLowerCase().includes(search.toLowerCase()) ||
-            product.description.toLowerCase().includes(search.toLowerCase())
-        ) : [];
+    function handleSortChange(filterInput) {
+        setSortValue(filterInput);
+        console.log(sortValue)
+    }
+
+    const filteredAndSortedProducts = useMemo(() => {
+        console.log("Search useMemo", search)
+
+        if (!Array.isArray(products)) {
+            return [];
+        }
+
+        const filteredProducts = Array.isArray(products) ?
+            products.filter(product =>
+                product.name.toLowerCase().includes(search.toLowerCase()) ||
+                product.description.toLowerCase().includes(search.toLowerCase())
+            ) : [];
+
+        return filteredProducts.sort((a, b) => {
+            switch (sortValue) {
+                case "Price Low to High":
+                    return parseFloat(a.price) - parseFloat(b.price);
+                case "Price High to Low":
+                    return parseFloat(b.price) - parseFloat(a.price);
+                case "Popularity":
+                default:
+                    return parseInt(b.popularity) - parseInt(a.popularity);
+            }
+        });
+    }, [search, sortValue, products])
 
     return (
         <div className="max-w-6xl mx-auto">
-            <div className={"flex flex-col sm:flex-row justify-between items-center gap-4 pt-12"}>
+            <div className="flex flex-col sm:flex-row justify-between items-center gap-4 pt-12">
                 <SearchBox
                     label={"Search"}
                     placeholder={"Search Products..."}
@@ -29,13 +57,14 @@ export default function ProductListings({products}) {
                 />
                 <Dropdown
                     label={"Sort by:"}
-                    options={[]}
+                    options={sortList}
+                    handleSort={(value) => handleSortChange(value)}
                 />
             </div>
             <div className="grid grid-col-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-8 gap-x-6 py-12">
                 {
-                    filteredProducts.length > 0 ?
-                        filteredProducts.map((product) => (
+                    filteredAndSortedProducts.length > 0 ?
+                        filteredAndSortedProducts.map((product) => (
                             <ProductCard key={product.productId} product={product}/>
                         ))
                         : (
